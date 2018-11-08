b8f7e797145c1c91b0730b4310adb54ff4ffd1d6
lnicosia <lnicosia@student.42.fr>
2018-11-08 12:47:11 +0100

    strrchr corrected.

--- src/ft_strrchr.c
+++ src/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 15:49:01 by lnicosia          #+#    #+#             */
-/*   Updated: 2018/11/07 15:52:21 by lnicosia         ###   ########.fr       */
+/*   Updated: 2018/11/08 12:46:25 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,24 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int		i;
-	int		j;
-	char	*str;
+	int	i;
+	int	res;
 
 	i = 0;
-	j = 0;
-	str = ft_strdup(s);
-	while (str[i])
+	res = 0;
+	if (!s[i])
+		return (0);
+	if (c == '\0')
+	{
+		while (s[i])
+			i++;
+		return ((char*)s + i);
+	}
+	while (s[i])
 	{
 		if (c == s[i])
-			j = i;
+			res = i;;
 		i++;
 	}
-	return (str + j);
+	return ((char*)s + res);
 }
