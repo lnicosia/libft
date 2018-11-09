f6bdddc47e2e9e489d9825f6478fe971be83ac53
lnicosia <lnicosia@student.42.fr>
2018-11-09 17:50:16 +0100

    lstdelone protected.

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 17:29:32 by lnicosia          #+#    #+#             */
-/*   Updated: 2018/11/09 17:40:14 by lnicosia         ###   ########.fr       */
+/*   Updated: 2018/11/09 17:44:27 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t))
 {
+	if (alst = NULL)
+		return ;
 	del((*alst)->content, (*alst)->content_size);
 	free(*alst);
 	*alst = NULL;
