452732d98fdf4595cda9e1b16d8395e559802c8e
lnicosia <lnicosia@student.42.fr>
2018-11-21 15:00:53 +0100

    strjoin malloced 1 byte more than necessary.

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 17:12:02 by lnicosia          #+#    #+#             */
-/*   Updated: 2018/11/09 15:26:22 by lnicosia         ###   ########.fr       */
+/*   Updated: 2018/11/21 14:59:02 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (0);
-	if (!(str = ft_strnew((ft_strlen(s1) + ft_strlen(s2) + 1))))
+	if (!(str = ft_strnew((ft_strlen(s1) + ft_strlen(s2)))))
 		return (0);
 	if (!(str = ft_strcpy(str, s1)))
 		return (0);
