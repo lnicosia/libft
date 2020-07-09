0c6e9d88768b4819fda5f639b13baaf87340b417
lnicosia <lnicosia@student.42.fr>
2020-07-09 11:44:54 +0200

    libft upadte with complexes.

--- complex.c
+++ complex.c
@@ -1,27 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   complex.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/05 17:47:40 by lnicosia          #+#    #+#             */
-/*   Updated: 2019/07/29 13:57:48 by lnicosia         ###   ########.fr       */
+/*   Created: 2019/03/12 17:05:34 by lnicosia          #+#    #+#             */
+/*   Updated: 2019/10/02 18:52:04 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+#include <math.h>
 
-size_t	ft_strlen(const char *str)
+t_complex	ft_cconj(t_complex c)
 {
-	size_t	res;
+	c.i = -c.i;
+	return (c);
+}
 
-	res = 0;
-	while (*str)
-	{
-		res++;
-		str++;
-	}
+t_complex	new_complex(double r, double i)
+{
+	t_complex	res;
+
+	res.r = r;
+	res.i = i;
+	return (res);
+}
+
+t_complex	ft_csin(t_complex c)
+{
+	t_complex	res;
+
+	res.r = sin(c.r) * cosh(c.i);
+	res.i = sinh(c.i) * cos(c.r);
+	return (res);
+}
+
+t_complex	ft_ccos(t_complex c)
+{
+	t_complex	res;
+
+	res.r = cos(c.r) * cosh(c.i);
+	res.i = -sinh(c.r) * sinh(c.r);
 	return (res);
 }
