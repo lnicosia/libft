fc3b0ea5213d397a84c11c4c068a45aaa8e97e87
lnicosia <lnicosia@student.42.fr>
2021-03-26 18:08:46 +0100

    [ADD] Added background colors, bold and invert mode to the possible output modes.#

--- ft_printf/color.h
+++ ft_printf/color.h
@@ -3,21 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   color.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lnicosia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: lnicosia <lnicosia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/14 14:15:45 by lnicosia          #+#    #+#             */
-/*   Updated: 2019/02/22 14:23:02 by lnicosia         ###   ########.fr       */
+/*   Updated: 2021/03/26 18:02:42 by lnicosia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COLOR_H
 # define COLOR_H
-# define RED "\033[0;31m"
-# define GREEN "\033[0;32m"
-# define YELLOW "\033[0;33m"
-# define BLUE "\033[0;34m"
-# define MAGENTA "\033[0;35m"
-# define CYAN "\033[0;36m"
 # define RESET "\033[0m"
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN "\033[36m"
+# define BRED "\033[41m"
+# define BGREEN "\033[42m"
+# define BYELLOW "\033[43m"
+# define BBLUE "\033[44m"
+# define BMAGENTA "\033[45m"
+# define BCYAN "\033[46m"
+# define BOLD "\033[1m"
+# define INVERT "\033[7m"
+# define UNDERLINE "\033[4m"
 
 #endif
