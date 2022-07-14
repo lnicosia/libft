432c98bef2e3fec9413543878a12e4aa35218c70
lnicosia <lnicosia@student.42.fr>
2022-07-14 09:22:44 +0200

    [ADD] first '--' ignored by is_arg_an_opt().

--- src/is_arg_an_opt.c
+++ src/is_arg_an_opt.c
@@ -3,12 +3,20 @@
 int		is_arg_an_opt(char **argv, int argi, const char *optstring,
 						const struct option *longopts)
 {
-	size_t	len = ft_strlen(argv[argi]);
+	static int	first_double = 1;
+	size_t		len = ft_strlen(argv[argi]);
 
 	//	Arg is exactly '--'
 	if (argv[argi][0] == '-' && argv[argi][1] == '-'
 		&& len == 2)
+	{
+		if (first_double == 1)
+		{
+			first_double = 0;
+			return 1;
+		}
 		return 0;
+	}
 	if (len > 1 && argv[argi][0] == '-')
 		return 1;
 
